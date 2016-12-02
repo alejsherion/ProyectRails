194c5c6028d68f5a9a41f843b2bf27531159dbae
= <=>
2016-12-01 20:10:49 -0500

    tasks view

---
+++ tasklist/app/controllers/tasks_controller.rb
@@ -0,0 +1,19 @@
+class TasksController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def delete
+  end
+end
--- tasklist/config/routes.rb
+++ tasklist/config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'tasks/new'
+
+  get 'tasks/index'
+
+  get 'tasks/edit'
+
+  get 'tasks/show'
+
+  get 'tasks/update'
+
+  get 'tasks/delete'
+
   root to: 'static_pages#welcome'
    devise_for :users, controllers: {
      sessions: 'users/sessions',
